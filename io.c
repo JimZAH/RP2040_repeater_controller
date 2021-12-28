8671b534e0f226501f1c1ca45c8fcaf681f16554
Jim <james@colderwood.com>
2021-12-28 19:00:40 +0000

    use clock

--- io.c
+++ io.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include "pico/stdlib.h"
 #include "hardware/pwm.h"
+#include "hardware/clocks.h"
 #include "io.h"
 #include "globals.h"
 
@@ -37,7 +38,7 @@ void set_pwm_pin(uint pin, uint freq, uint duty_c) { // duty_c between 0..10000
 		gpio_set_function(pin, GPIO_FUNC_PWM);
 		uint slice_num = pwm_gpio_to_slice_num(pin);
         pwm_config config = pwm_get_default_config();
-		float div = (float)133000000L / (freq * 10000);
+		float div = (float)clock_get_hz(clk_sys) / (freq * 10000);
 		pwm_config_set_clkdiv(&config, div);
 		pwm_config_set_wrap(&config, 10000);
 		pwm_init(slice_num, &config, true); // start the pwm running according to the config

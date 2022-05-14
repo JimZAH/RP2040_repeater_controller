e209e06cf5438127c3c1b2af04b4b567ffbd1acd
Jim <james@colderwood.com>
2022-05-14 23:04:10 +0100

    Added tx state msg

--- serial.h
+++ serial.h
@@ -2,7 +2,8 @@
 
 #define p_size 8
 
-const uint8_t boot_msg[p_size] = {0x4, 0x0, 0x0, 0x0, 'b', 'o', 'o', 't'};
+uint8_t boot_msg[p_size] = {0x4, 0x0, 0x0, 0x0, 'b', 'o', 'o', 't'};
+uint8_t state_tx[p_size] = {0x2, 0x0, 0x0, 0x0, 0x0, 0x0, 't', 'x'};
 
 void usend(uint8_t tx_buff[]){
     uart_write_blocking(uart0,tx_buff,p_size);
